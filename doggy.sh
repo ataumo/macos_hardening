1ca2da6759c7c6b09cd5ee9653c21edb01d96780
ataumo <l.coumau@yahoo.fr>
2022-09-09 19:11:38 +0200

    Add sudo asking and System Preferences quitting

--- doggy.sh
+++ doggy.sh
@@ -390,6 +390,12 @@ fi
 #                                                                              #
 ################################################################################
 
+# Close any open System Preferences panes, to prevent them from overriding settings we’re about to change
+osascript -e 'tell application "System Preferences" to quit'
+
+# Ask for the administrator password upfront
+sudo -v
+
 #
 # Print Intro
 #
